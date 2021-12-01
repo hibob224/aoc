cf0afd4990550da62a1711a9ce6bd70ee1a1f1e9
Jonathan Miller <contact@jmiller.xyz>
2021-12-01 07:07:43 +0000

    Day 1
    2021, here we go again

---
+++ src/main/kotlin/y2021/day01/Day01.kt
@@ -0,0 +1,30 @@
+package y2021.day01
+
+import java.io.File
+
+fun main() {
+    println("Part one: ${Day01.solvePartOne()}")
+    println("Part two: ${Day01.solvePartTwo()}")
+}
+
+object Day01 {
+
+    private val directory: String
+        get() = this::class.java.`package`.name.replace('.', '/')
+
+    private val input =
+        File("src/main/kotlin/$directory/input.txt")
+            .readLines()
+            .map { it.toInt() }
+
+    fun solvePartOne(): Int = input
+        .windowed(2)
+        .filter { it[1] > it[0] }
+        .size
+
+    fun solvePartTwo(): Int = input.windowed(3)
+        .map(List<Int>::sum)
+        .windowed(2)
+        .filter { it[1] > it[0] }
+        .size
+}
---
+++ src/test/kotlin/y2021/day01/Day01Test.kt
@@ -0,0 +1,18 @@
+package y2021.day01
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day01Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals(1292, Day01.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(1262, Day01.solvePartTwo())
+    }
+}
