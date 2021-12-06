7a6c8f7c3e33bebbfd880a663e566c2ab114c4ff
Jonathan Miller <contact@jmiller.xyz>
2021-12-06 15:56:46 +0000

    Day 6

---
+++ src/main/kotlin/y2021/day06/Day06.kt
@@ -0,0 +1,47 @@
+package y2021.day06
+
+import java.io.File
+
+fun main() {
+    println("Part one: ${Day06.solvePartOne()}")
+    println("Part two: ${Day06.solvePartTwo()}")
+}
+
+object Day06 {
+
+    private val directory: String
+        get() = this::class.java.`package`.name.replace('.', '/')
+
+    private val input: Map<Int, Long> = File("src/main/kotlin/$directory/input.txt")
+        .readText()
+        .split(",")
+        .map(String::toInt)
+        .groupingBy { it }
+        .eachCount()
+        .map { it.key to it.value.toLong() }
+        .toMap()
+
+    fun solvePartOne(): Long = simulateGenerations(input, 80).values.sum()
+
+    fun solvePartTwo(): Long = simulateGenerations(input, 256).values.sum()
+
+    private fun simulateGenerations(start: Map<Int, Long>, generations: Int): Map<Int, Long> {
+        var currentGen = start
+        repeat(generations) { currentGen = generation(currentGen) }
+        return currentGen
+    }
+
+    private fun generation(current: Map<Int, Long>): Map<Int, Long> {
+        val newGeneration = mutableMapOf<Int, Long>()
+        current.forEach {
+            when (it.key) {
+                0 -> { // Reproduce
+                    newGeneration[6] = newGeneration.getOrDefault(6, 0) + it.value
+                    newGeneration[8] = it.value
+                }
+                else -> newGeneration[it.key.dec()] = newGeneration.getOrDefault(it.key.dec(), 0) + it.value
+            }
+        }
+        return newGeneration
+    }
+}
---
+++ src/test/kotlin/y2021/day06/Day06Test.kt
@@ -0,0 +1,18 @@
+package y2021.day06
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day06Test {
+
+    @Test
+    fun solvePartOne() {
+        assertEquals(379114, Day06.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(1702631502303, Day06.solvePartTwo())
+    }
+}
