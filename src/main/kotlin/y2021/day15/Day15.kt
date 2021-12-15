a4c2dd2ef8d8dc6b16a448c9ee424772f23d4d0e
Jonathan Miller <contact@jmiller.xyz>
2021-12-15 09:42:01 +0000

    Day 15 P2
    Needs a spruce up

--- src/main/kotlin/y2021/day15/Day15.kt
+++ src/main/kotlin/y2021/day15/Day15.kt
@@ -67,6 +67,77 @@ object Day15 {
     }
 
     fun solvePartTwo(): Int {
-        return -1
+        val input = parseInput()
+        val grid = mutableMapOf<Point, Int>()
+        val tileWidth = input.first().length
+        val tileHeight = input.size
+
+        val tempGrid = input.map { row ->
+            row.map { it.toString().toInt() }.toMutableList()
+        }.toMutableList()
+
+        // Enlarge the grid...
+        val newWidth = tileWidth * 5
+        val newHeight = tileWidth * 5
+        val newerGrid = List(newHeight) { List(newWidth) { 0 }.toMutableList() }
+
+        (0 until newHeight).forEach { y ->
+            (0 until newWidth).forEach { x ->
+                val checkX = x % tileWidth
+                val checkY = y % tileHeight
+                val addX = x / tileWidth
+                val addY = y / tileHeight
+                newerGrid[y][x] = if (tempGrid[checkY][checkX] + addX + addY > 9) {
+                    tempGrid[checkY][checkX] + addX + addY - 9
+                } else {
+                    tempGrid[checkY][checkX] + addX + addY
+                }
+            }
+        }
+
+        newerGrid.forEachIndexed { y, row ->
+            row.forEachIndexed { x, i ->
+                grid[Point(x, y)] = i
+            }
+        }
+
+        val start = Point(0, 0)
+        val target = Point(x = grid.maxOf { it.key.x }, y = grid.maxOf { it.key.y })
+        val openSet = mutableSetOf(start)
+        val cameFrom = mutableMapOf<Point, Point>()
+        val gScore = mutableMapOf(start to 0)
+        val fScore = mutableMapOf(start to Integer.MAX_VALUE)
+
+        while (openSet.isNotEmpty()) {
+            val current = openSet.minByOrNull { fScore[it]!! }!!
+            if (current == target) {
+                val path = mutableListOf(current)
+                var next = current
+                while (next in cameFrom.keys) {
+                    next = cameFrom[next]!!
+                    path.add(0, next)
+                }
+                return path.subList(fromIndex = 1, toIndex = path.size).sumBy { grid[it]!! }
+            }
+
+            openSet.remove(current)
+
+            current.getNeighbours()
+                .filter { it.x in 0..target.x && it.y in 0..target.y }
+                .forEach { neighbour ->
+                    val tentativeGScore = gScore[current]!! + grid[neighbour]!!
+                    if (tentativeGScore < gScore.getOrDefault(neighbour, Integer.MAX_VALUE)) {
+                        cameFrom[neighbour] = current
+                        gScore[neighbour] = tentativeGScore
+                        fScore[neighbour] = tentativeGScore + neighbour.manhattan(target)
+                        if (neighbour !in openSet) {
+                            openSet += neighbour
+                        }
+                    }
+                }
+
+        }
+
+        throw IllegalStateException("How")
     }
 }
--- src/test/kotlin/y2021/day15/Day15Test.kt
+++ src/test/kotlin/y2021/day15/Day15Test.kt
@@ -13,6 +13,6 @@ internal class Day15Test {
 
     @Test
     fun solvePartTwo() {
-        assertEquals(-1, Day15.solvePartTwo())
+        assertEquals(2864, Day15.solvePartTwo())
     }
 }
