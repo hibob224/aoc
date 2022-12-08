c42022d472d474d74dd5a4da4ec18c994936eee6
Jonathan Miller <contact@jmiller.xyz>
2022-12-08 10:16:29 +0000

    Day 8

---
+++ src/main/kotlin/y2022/day08/Day08.kt
@@ -0,0 +1,79 @@
+package y2022.day08
+
+import utils.Point
+import java.io.File
+
+fun main() {
+    println("Part one: ${Day08.solvePartOne()}")
+    println("Part two: ${Day08.solvePartTwo()}")
+}
+
+object Day08 {
+
+    private val directory: String
+        get() = this::class.java.`package`.name.replace('.', '/')
+
+    private val input = File("src/main/kotlin/$directory/input.txt")
+        .readLines()
+        .also {
+            height = it.lastIndex
+            width = it.first().lastIndex
+        }
+        .flatMapIndexed { y, s ->
+            s.mapIndexed { x, c ->
+                Point(x, y) to c.digitToInt()
+            }
+        }.toMap()
+    private var width = 0
+    private var height = 0
+
+    fun solvePartOne(): Int {
+        var visibleCount = 0
+
+        input.forEach { (point, pointHeight) ->
+            if (point.x == 0 || point.x == width || point.y == 0 || point.y == height) {
+                visibleCount++
+                return@forEach
+            }
+
+            val visibleLeft = (0..point.x.dec()).mapNotNull { input[Point(it, point.y)] }.all { it < pointHeight }
+            val visibleRight = (point.x.inc()..width).mapNotNull { input[Point(it, point.y)] }.all { it < pointHeight }
+            val visibleUp = (0..point.y.dec()).mapNotNull { input[Point(point.x, it)] }.all { it < pointHeight }
+            val visibleDown = (point.y.inc()..height).mapNotNull { input[Point(point.x, it)] }.all { it < pointHeight }
+            if (visibleUp || visibleDown || visibleLeft || visibleRight) {
+                visibleCount++
+            }
+        }
+
+
+        return visibleCount
+    }
+
+    fun solvePartTwo(): Int {
+        val scenicScores = mutableListOf<Int>()
+        input.forEach { (point, pointHeight) ->
+            var visibleLeft = 0
+            if (point.x > 0) {
+                val trees = (0..point.x.dec()).mapNotNull { input[Point(it, point.y)] }.reversed()
+                visibleLeft = trees.indexOfFirst { it >= pointHeight }.takeIf { it > -1 }?.inc() ?: trees.size
+            }
+            var visibleRight = 0
+            if (point.x < width) {
+                val trees = (point.x.inc()..width).mapNotNull { input[Point(it, point.y)] }
+                visibleRight = trees.indexOfFirst { it >= pointHeight }.takeIf { it > -1 }?.inc() ?: trees.size
+            }
+            var visibleUp = 0
+            if (point.y > 0) {
+                val trees = (0..point.y.dec()).mapNotNull { input[Point(point.x, it)] }.reversed()
+                visibleUp = trees.indexOfFirst { it >= pointHeight }.takeIf { it > -1 }?.inc() ?: trees.size
+            }
+            var visibleDown = 0
+            if (point.x < width) {
+                val trees = (point.y.inc()..height).mapNotNull { input[Point(point.x, it)] }
+                visibleDown = trees.indexOfFirst { it >= pointHeight }.takeIf { it > -1 }?.inc() ?: trees.size
+            }
+            scenicScores += visibleLeft * visibleRight * visibleUp * visibleDown
+        }
+        return scenicScores.max()
+    }
+}
---
+++ src/test/kotlin/y2022/day08/Day08Test.kt
@@ -0,0 +1,17 @@
+package y2022.day08
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day08Test {
+    @Test
+    fun solvePartOne() {
+        assertEquals(1647, Day08.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(392080, Day08.solvePartTwo())
+    }
+}
