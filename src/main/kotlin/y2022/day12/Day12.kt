d277b3b31f066778d0f9dc9d88569f593e188fc7
Jonathan Miller <contact@jmiller.xyz>
2022-12-12 20:27:26 +0000

    Day 12

---
+++ src/main/kotlin/y2022/day12/Day12.kt
@@ -0,0 +1,84 @@
+package y2022.day12
+
+import utils.Point
+import java.io.File
+
+fun main() {
+    println("Part one: ${Day12.solvePartOne()}")
+    println("Part two: ${Day12.solvePartTwo()}")
+}
+
+object Day12 {
+
+    private val directory: String
+        get() = this::class.java.`package`.name.replace('.', '/')
+
+    private lateinit var start: Point
+    private lateinit var target: Point
+    private val input = File("src/main/kotlin/$directory/input.txt")
+        .readLines()
+        .flatMapIndexed { y: Int, row: String ->
+            row.mapIndexed { x, level ->
+                val point = Point(x, y)
+                val mappedLevel = when (level) {
+                    'S' -> {
+                        start = point
+                        'a'
+                    }
+
+                    'E' -> {
+                        target = point
+                        'z'
+                    }
+                    else -> level
+                }
+                point to mappedLevel
+            }
+        }
+        .toMap()
+
+    fun solvePartOne(): Int = solve(start)
+
+    // Bored, let's just run solve on all possible starts, all 1000 of 'em
+    fun solvePartTwo(): Int = input.filterValues { it == 'a' }.minOf { solve(it.key) }
+
+    // A-Star
+    private fun solve(start: Point): Int {
+        val openSet = mutableSetOf(start)
+        val cameFrom = mutableMapOf<Point, Point>()
+        val gScore = mutableMapOf(start to 0)
+        val fScore = mutableMapOf(start to Integer.MAX_VALUE)
+
+        while (openSet.isNotEmpty()) {
+            val current = openSet.minByOrNull { fScore[it]!! }!!
+            if (current == target) {
+                val path = mutableListOf(current)
+                var next = current
+                while (next in cameFrom.keys) {
+                    next = cameFrom[next]!!
+                    path.add(0, next)
+                }
+                return path.size.dec()
+            }
+
+            openSet.remove(current)
+            val currentHeight = input[current]!!
+            current.getNeighbours()
+                .filter { it in input.keys }
+                .filter { input[it]!! - currentHeight <= 1 }
+                .forEach { neighbour ->
+                    val tentativeGScore = gScore[current]!! + 1
+                    if (tentativeGScore < gScore.getOrDefault(neighbour, Integer.MAX_VALUE)) {
+                        cameFrom[neighbour] = current
+                        gScore[neighbour] = tentativeGScore
+                        fScore[neighbour] = tentativeGScore + neighbour.manhattan(target)
+                        if (neighbour !in openSet) {
+                            openSet += neighbour
+                        }
+                    }
+                }
+
+        }
+        return Integer.MAX_VALUE
+    }
+}
---
+++ src/test/kotlin/y2022/day12/Day12Test.kt
@@ -0,0 +1,17 @@
+package y2022.day12
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day12Test {
+    @Test
+    fun solvePartOne() {
+        assertEquals(350, Day12.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(349, Day12.solvePartTwo())
+    }
+}
