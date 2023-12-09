253118da8676a8f28134cd023f0d712345c1e2dc
Jonathan Miller <contact@jmiller.xyz>
2023-12-09 11:53:48 +0000

    Day 9

---
+++ src/main/kotlin/y2023/day09/Day09.kt
@@ -0,0 +1,33 @@
+package y2023.day09
+
+import utils.getInputFile
+
+fun main() {
+    println("Part one: ${Day09.solvePartOne()}")
+    println("Part two: ${Day09.solvePartTwo()}")
+}
+
+object Day09 {
+
+    private val input =
+        getInputFile(this::class.java.packageName, example = false)
+            .readLines()
+            .map { it.split(" ").map(String::toLong) }
+            .map {
+                buildList<List<Long>> {
+                    add(it)
+
+                    while (last().any { it != 0L }) {
+                        add(last().windowed(2).map { it[1] - it[0] })
+                    }
+                }
+            }
+
+    fun solvePartOne(): Long =
+        input.sumOf { it.sumOf(List<Long>::last) }
+
+    fun solvePartTwo(): Long =
+        input
+            .map { it.map(List<Long>::first).reversed() }
+            .sumOf { it.reduce { acc, l -> l - acc } }
+}
---
+++ src/test/kotlin/y2023/day09/Day09Test.kt
@@ -0,0 +1,16 @@
+package y2023.day09
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day09Test {
+    @Test
+    fun solvePartOne() {
+        assertEquals(1938800261, Day09.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(1112, Day09.solvePartTwo())
+    }
+}
