d0069f442b4f10ceb17a5a72973cea04ed5840ff
Jonathan Miller <contact@jmiller.xyz>
2023-12-11 11:24:12 +0000

    Day 11 P1

---
+++ src/main/kotlin/y2023/day11/Day11.kt
@@ -0,0 +1,47 @@
+package y2023.day11
+
+import utils.Point
+import utils.combinations
+import utils.getInputFile
+
+fun main() {
+    println("Part one: ${Day11.solvePartOne()}")
+    println("Part two: ${Day11.solvePartTwo()}")
+}
+
+object Day11 {
+
+    private val input =
+        getInputFile(this::class.java.packageName, example = false)
+            .readLines()
+            .flatMapIndexed { y, line ->
+                line.mapIndexed { x, c -> Point(x, y) to c }
+            }.toMap()
+
+    fun solvePartOne(): Long {
+        val maxY = input.maxOf { it.key.y }
+        val maxX = input.maxOf { it.key.x }
+
+        val emptyRows = (0..maxY).filter { y ->
+            input.filterKeys { it.y == y }.all { it.value == '.' }
+        }
+        val emptyCols = (0..maxX).filter { x ->
+            input.filterKeys { it.x == x }.all { it.value == '.' }
+        }
+
+        val galaxies = input
+            .filterValues { it == '#' }
+            .keys
+            .combinations(2)
+            .map {
+                val (pointA, pointB) = it
+                val manhattan = pointA.manhattan(pointB)
+                val extraRows = emptyRows.count { it in minOf(pointA.y, pointB.y)..maxOf(pointA.y, pointB.y) }
+                val extraCols = emptyCols.count { it in minOf(pointA.x, pointB.x)..maxOf(pointA.x, pointB.x) }
+                manhattan + extraRows + extraCols
+            }
+        return galaxies.sum().toLong()
+    }
+
+    fun solvePartTwo(): Long = 0
+}
---
+++ src/test/kotlin/y2023/day11/Day11Test.kt
@@ -0,0 +1,16 @@
+package y2023.day11
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day11Test {
+    @Test
+    fun solvePartOne() {
+        assertEquals(9609130, Day11.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(0, Day11.solvePartTwo())
+    }
+}
