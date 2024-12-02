25969b1b484b3ce8a15f17c84ddc3b982367e857
Jonathan Miller <contact@jmiller.xyz>
2024-12-02 23:45:25 +0000

    Day 2 improvement

--- src/main/kotlin/y2024/day02/Day02.kt
+++ src/main/kotlin/y2024/day02/Day02.kt
@@ -1,7 +1,6 @@
 package y2024.day02
 
 import utils.getInputFile
-import kotlin.math.absoluteValue
 
 fun main() {
     println("Part one: ${Day02.solvePartOne()}")
@@ -12,32 +11,25 @@ object Day02 {
 
     private val input = getInputFile(this::class.java.packageName)
         .readLines()
-        .map { it.split(' ').map { it.toInt() } }
+        .map { it.split(' ').map(String::toInt) }
 
     fun solvePartOne(): Long = input.count(::isValid).toLong()
 
     fun solvePartTwo(): Long {
-        return input.count {
-            if (isValid(it)) {
-                true
-            } else {
-                // Can't be bothered doing something smart to figure out which can be removed to fix things, so just try every option
-                repeat(it.size) { i ->
-                    val edited = it.toMutableList().also { it.removeAt(i) }
-                    if (isValid(edited)) {
-                        return@count true
-                    }
-                }
-                false
+        return input.count { report ->
+            report.indices.any {
+                isValid(report.toMutableList().apply { removeAt(it) })
             }
         }.toLong()
     }
 
     private fun isValid(list: List<Int>): Boolean {
-        val allAscDesc = list.sorted() == list || list.sortedDescending() == list
-        val diff = list
-            .windowed(2)
-            .all { (it[0] - it[1]).absoluteValue in 1..3 }
-        return allAscDesc && diff
+        val allAsc = list
+            .zipWithNext { a, b -> (a - b) }
+            .all { it in 1..3 }
+        val allDesc = list
+            .zipWithNext { a, b -> (a - b) }
+            .all { it in -1 downTo -3 }
+        return allAsc || allDesc
     }
 }
