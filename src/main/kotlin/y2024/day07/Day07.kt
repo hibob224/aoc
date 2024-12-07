d53b5145c3434e1473c26793e9f0777279553a99
Jonathan Miller <contact@jmiller.xyz>
2024-12-07 12:52:54 +0000

    Combining solutions

--- src/main/kotlin/y2024/day07/Day07.kt
+++ src/main/kotlin/y2024/day07/Day07.kt
@@ -7,6 +7,8 @@ fun main() {
     println("Part two: " + Day07.solvePartTwo())
 }
 
+private fun Long.concat(other: Long): Long = "$this$other".toLong()
+
 object Day07 {
 
     private val input = getInputFile(this::class.java.packageName, example = false)
@@ -16,30 +18,26 @@ object Day07 {
             targ.toLong() to nums.split(" ").map(String::toLong)
         }
 
-    fun solvePartOne(): Long {
-        return input
-            .filter { eval(it.second.first(), it.first, it.second, 1) }
-            .sumOf { it.first }
-    }
+    fun solvePartOne(): Long = solve(listOf(Long::plus, Long::times))
 
-    fun solvePartTwo(): Long {
-        return input
-            .filter { eval2(it.second.first(), it.first, it.second, 1) }
-            .sumOf { it.first }
-    }
+    fun solvePartTwo(): Long = solve(listOf(Long::plus, Long::times, Long::concat))
 
-    private fun eval(acc: Long, target: Long, nums: List<Long>, index: Int): Boolean {
+    private fun solve(operations: List<Long.(Long) -> Long>): Long =
+        input
+            .filter { eval(it.second.first(), it.first, it.second, 1, operations) }
+            .sumOf { it.first }
+
+    private fun eval(
+        acc: Long,
+        target: Long,
+        nums: List<Long>,
+        index: Int,
+        operations: List<Long.(Long) -> Long>,
+    ): Boolean {
         if (index > nums.lastIndex) return acc == target
         if (acc > target) return false
-        return eval(acc + nums[index], target, nums, index.inc()) ||
-            eval(acc * nums[index], target, nums, index.inc())
-    }
-
-    private fun eval2(acc: Long, target: Long, nums: List<Long>, index: Int): Boolean {
-        if (index > nums.lastIndex) return acc == target
-        if (acc > target) return false
-        return eval2(acc + nums[index], target, nums, index.inc()) ||
-            eval2(acc * nums[index], target, nums, index.inc()) ||
-            eval2("${acc}${nums[index]}".toLong(), target, nums, index.inc())
+        return operations
+            .map { it(acc, nums[index]) }
+            .any { eval(it, target, nums, index.inc(), operations) }
     }
 }
