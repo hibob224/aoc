08fed6b682db52a6b26e0f2a3e567ad52ddbe8e6
Jonathan Miller <contact@jmiller.xyz>
2024-12-10 09:21:17 +0000

    Day 10

---
+++ src/main/kotlin/y2024/day10/Day10.kt
@@ -0,0 +1,45 @@
+package y2024.day10
+
+import utils.Point
+import utils.getInputFile
+import utils.toPointGrid
+
+fun main() {
+    println("Part one: " + Day10.solvePartOne())
+    println("Part two: " + Day10.solvePartTwo())
+}
+
+object Day10 {
+
+    private val input =
+        getInputFile(this::class.java.packageName, example = false)
+            .readLines()
+            .toPointGrid { _, c -> c.digitToInt() }
+
+    fun solvePartOne(): Int {
+        return input
+            .filterValues { it == 0 }
+            .keys
+            .sumOf { trailheads(it).toSet().size }
+    }
+
+    fun solvePartTwo(): Int {
+        return input
+            .filterValues { it == 0 }
+            .keys
+            .sumOf { trailheads(it).size }
+    }
+
+    private fun trailheads(point: Point): List<Point> {
+        val currentHeight = input[point]!!
+        val neighbours = point
+            .getNeighbours()
+            .filter { input[it] == currentHeight + 1 }
+
+        return if (currentHeight == 8) {
+            neighbours
+        } else {
+            neighbours.flatMap { trailheads(it) }
+        }
+    }
+}
---
+++ src/test/kotlin/y2024/day10/Day10Test.kt
@@ -0,0 +1,16 @@
+package y2024.day10
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day10Test {
+    @Test
+    fun solvePartOne() {
+        assertEquals(794, Day10.solvePartOne())
+    }
+
+    @Test
+    fun solvePartTwo() {
+        assertEquals(1706, Day10.solvePartTwo())
+    }
+}
