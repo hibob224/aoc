53cc898796942bae3ab375547cfd2252a3a28ca2
Jonathan Miller <contact@jmiller.xyz>
2025-12-09 22:21:15 +0000

    Condense

--- src/main/kotlin/y2025/day09/Day09.kt
+++ src/main/kotlin/y2025/day09/Day09.kt
@@ -16,28 +16,25 @@ class Day09 : Puzzle<Long, Long>(2025, 9, example = false) {
         .lines()
         .map { it.split(",") }
         .map { (x, y) -> Point(x.toInt(), y.toInt()) }
+    private val rects = input
+        .combinations(2)
+        .map { (a, b) ->
+            Rectangle2D.Double(
+                minOf(a.x, b.x).toDouble(),
+                minOf(a.y, b.y).toDouble(),
+                (b.x - a.x).absoluteValue.toDouble(),
+                (b.y - a.y).absoluteValue.toDouble(),
+            )
+        }
 
     override fun solvePartOne(): Long =
-        input.combinations(2)
-            .maxOf { (a, b) ->
-                (a.x - b.x + 1).toLong() * (a.y - b.y + 1).toLong()
-            }
+        rects
+            .maxOf { it.width.inc() * it.height.inc() }
+            .toLong()
 
     override fun solvePartTwo(): Long {
         val polygon = Polygon()
-        input.forEach { (x, y) ->
-            polygon.addPoint(x, y)
-        }
-        val rects = input
-            .combinations(2)
-            .map { (a, b) ->
-                Rectangle2D.Double(
-                    minOf(a.x, b.x).toDouble(),
-                    minOf(a.y, b.y).toDouble(),
-                    (b.x - a.x).absoluteValue.toDouble(),
-                    (b.y - a.y).absoluteValue.toDouble(),
-                )
-            }
+        input.forEach { (x, y) -> polygon.addPoint(x, y) }
         return rects
             .filter { it in polygon }
             .maxOf { it.width.inc() * it.height.inc() }
